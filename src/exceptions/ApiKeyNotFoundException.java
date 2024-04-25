a36b9f751b91dfbb15bfc0bdada67a622e6a10aa
Fredy Amaya <feat0429f@gmail.com>
2024-04-25 12:50:28 -0600

    Modified 'Exceptions' folder name to 'exceptions'.

--- src/exceptions/ApiKeyNotFoundException.java
+++ src/exceptions/ApiKeyNotFoundException.java
@@ -1,4 +1,4 @@
-package Exceptions;
+package exceptions;
 
 public class ApiKeyNotFoundException extends RuntimeException {
     private final String message;
--- src/service/ConversionRateService.java
+++ src/service/ConversionRateService.java
@@ -1,6 +1,6 @@
 package service;
 
-import Exceptions.ApiKeyNotFoundException;
+import exceptions.ApiKeyNotFoundException;
 import com.google.gson.*;
 import dto.ConversionRateDto;
 
